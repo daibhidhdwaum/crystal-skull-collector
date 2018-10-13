9d987a50666d60adee2c2e26f97b298eda5060a6
David Downie <daviddownie@gmail.com>
2018-10-12 21:20:10 -0400

    edited css

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+"https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"
+
+$(document).ready(function(){
+    
+var score = 0;
+var wins = 0;
+var losses = 0;
+var computerOutput
+
+$("#ran-num");
+
+console.log("output");
+
+});
