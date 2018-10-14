c13f23056f2261ef08e2247f454feeaf0961a570
David Downie <daviddownie@gmail.com>
2018-10-14 13:25:33 -0400

    randomly generated number between 19 and 120

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,71 @@
-"https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"
-
 $(document).ready(function(){
     
-var score = 0;
-var wins = 0;
-var losses = 0;
-var computerOutput
+    var score = 0;
+    var wins = 0;
+    var losses = 0;
+    var targetNumber = "";
+    var computerOutput
+    var playerInput
 
-$("#ran-num");
+    //console.log("ready!");
 
-console.log("output");
+     //initiates game
+     $(".btn").on("click", function(){
+     //console.log("start");
+
+    
+        targetNumber = Math.floor(Math.random() * 101) + 19;
+        console.log(targetNumber);
+        $("#ran-num").html(targetNumber);
+
+    
+        
+        $("#wins-losses").html("<p>wins:</p><p>losses:</p>");
+        $("#wins-losses")
+        
+        
+
+        if(playerInput === targetNumber){
+            wins++
+            newGame();
+        } else {
+
+        }
+
+
+    });
+
+
+
+    for (var i = 0; i < 4; i++){
+
+        var skullNumber = Math.floor(Math.random() * 11) + 1;
+    //    console.log(random);
+    $("#skulls").append(skullNumber);
+
+    
+
+    }
+    //randomly generates a number between 1 and 12
+    /*$("#skull").on("click", function(){
+        var Skull = Math.floor(Math.random() * 12) + 1;
+        $("#scorecard").text(Skull);
+    });
+
+    $("#green-skull").on("click", function(){
+        var greenSkull = Math.floor(Math.random() * 12) + 1;
+        $("#scorecard").text(greenSkull);
+    });
+
+    $("#black-skull").on("click", function(){
+        var blackSkull = Math.floor(Math.random() * 12) + 1;
+        $("#scorecard").text(blackSkull);
+    });
+
+    $("#red-skull").on("click", function(){
+        var redSkull = Math.floor(Math.random() * 12) + 1;
+        $("#scorecard").text(redSkull);
+    });*/
+
 
 });
