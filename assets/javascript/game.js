eeda866b83a90a5405712324c35a7f404aaadae0
David Downie <daviddownie@gmail.com>
2018-10-14 17:44:06 -0400

    cleaned up code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,46 +5,66 @@ $(document).ready(function(){
     var losses = 0;
     var targetNumber = "";
     var computerOutput
-    var playerInput
 
-    //console.log("ready!");
 
-     //initiates game
-     $(".btn").on("click", function(){
-     //console.log("start");
+    //shows game instructions and default page
+    function start(){
+        $("#ran-num").html("<p>Try to match the randomly generated number using the skulls. Push start to play.</p>")
+        $("p").css("fontSize", "16px");
+        $("#scorecard").html("");
+    };
 
+    //sets target number
+    function target(){
+        computerOutput = Math.floor(Math.random() * 101) + 19;
+        $("#ran-num").html(computerOutput);
+        //console.log(computerOutput);
+    }
+
+    //sets new game
+    function newGame(){
+        score = 0;
+        target();
+    }
     
-        targetNumber = Math.floor(Math.random() * 101) + 19;
-        console.log(targetNumber);
-        $("#ran-num").html(targetNumber);
+    //game functionality
+    start();
 
+    $(".btn").on("click", function(){
+
+        $(".btn").hide();
+        computerOutput = Math.floor(Math.random() * 101) + 19;
+        $("#ran-num").html(computerOutput);
+        $("p").css("fontSize", "16px");
+        $("#wins-losses").html("<p>wins and losses</p>" + "<p></p>" + "<p>wins:</p>" + wins + "<p></p>" + "<p>losses:</p>" + losses);
+        $("#scorecard").html("<p>Your score is...</p>");
     
-        
-        $("#wins-losses").html("<p>wins:</p><p>losses:</p>");
-        $("#wins-losses")
-        
-        
 
-        if(playerInput === targetNumber){
-            wins++
-            newGame();
-        } else {
+            if(score === targetNumber){
+                wins++
+                newGame();
+            } else {
 
-        }
+            }
+});
+    
+    
+    
 
 
+    //randomly generates four numbers 
+
+
+    function playerInput() {
+        $("#skull").on("click",function(){
+        for (var i = 0; i < 4; i++){
+        var skull = Math.floor(Math.random() * 12) + 1;
+          console.log(skull);
+    $("#scorecard").text(skull);
+        };
     });
-
-
-
-    for (var i = 0; i < 4; i++){
-
-        var skullNumber = Math.floor(Math.random() * 11) + 1;
-    //    console.log(random);
-    $("#skulls").append(skullNumber);
-
-    
-
+    playerInput();
+    console.log(playerInput);
     }
     //randomly generates a number between 1 and 12
     /*$("#skull").on("click", function(){
