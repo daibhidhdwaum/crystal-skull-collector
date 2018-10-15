8d2cc6e3ec325e3b0d82aa6383a07b2dcb96607d
David Downie <daviddownie@gmail.com>
2018-10-14 22:40:10 -0400

    skulls are randomly generating a number

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,11 +5,14 @@ $(document).ready(function(){
     var losses = 0;
     var targetNumber = "";
     var computerOutput
-
-
+    var agate = Math.floor(Math.random() * 12) + 1;
+    var green = Math.floor(Math.random() * 12) + 1;
+    var black = Math.floor(Math.random() * 12) + 1;
+    var red = Math.floor(Math.random() * 12) + 1;
+    
     //shows game instructions and default page
     function start(){
-        $("#ran-num").html("<p>Try to match the randomly generated number using the skulls. Push start to play.</p>")
+        $("#ran-num").html("<p>Skulls generate a number each game. Use skull buttons to try and match the randomly generated number. Push start to play.</p>")
         $("p").css("fontSize", "16px");
         $("#scorecard").html("");
     };
@@ -18,7 +21,6 @@ $(document).ready(function(){
     function target(){
         computerOutput = Math.floor(Math.random() * 101) + 19;
         $("#ran-num").html(computerOutput);
-        //console.log(computerOutput);
     }
 
     //sets new game
@@ -26,66 +28,48 @@ $(document).ready(function(){
         score = 0;
         target();
     }
-    
+
     //game functionality
     start();
 
     $(".btn").on("click", function(){
-
+        target();
         $(".btn").hide();
-        computerOutput = Math.floor(Math.random() * 101) + 19;
-        $("#ran-num").html(computerOutput);
         $("p").css("fontSize", "16px");
         $("#wins-losses").html("<p>wins and losses</p>" + "<p></p>" + "<p>wins:</p>" + wins + "<p></p>" + "<p>losses:</p>" + losses);
         $("#scorecard").html("<p>Your score is...</p>");
-    
+
+    });
+
+     //skulls randomly select number between 1 and 12
+     $(".agate").on("click", function(){
+        $("#scorecard").html(agate);
+        console.log(agate);
+    });
+    $(".green").on("click", function(){
+        $("#scorecard").html(green);
+        console.log(green);
+    });
+    $(".black").on("click", function(){
+        $("#scorecard").html(black);
+        console.log(black);
+    });
+    $(".red").on("click", function(){
+        $("#scorecard").html(red);
+        console.log(red);
+    });
+
+        //playerInput();
 
             if(score === targetNumber){
                 wins++
                 newGame();
-            } else {
+            } else if (score > targetNumber){
+                losses++
+                newGame();
+            }else {
 
             }
 });
-    
-    
-    
 
 
-    //randomly generates four numbers 
-
-
-    function playerInput() {
-        $("#skull").on("click",function(){
-        for (var i = 0; i < 4; i++){
-        var skull = Math.floor(Math.random() * 12) + 1;
-          console.log(skull);
-    $("#scorecard").text(skull);
-        };
-    });
-    playerInput();
-    console.log(playerInput);
-    }
-    //randomly generates a number between 1 and 12
-    /*$("#skull").on("click", function(){
-        var Skull = Math.floor(Math.random() * 12) + 1;
-        $("#scorecard").text(Skull);
-    });
-
-    $("#green-skull").on("click", function(){
-        var greenSkull = Math.floor(Math.random() * 12) + 1;
-        $("#scorecard").text(greenSkull);
-    });
-
-    $("#black-skull").on("click", function(){
-        var blackSkull = Math.floor(Math.random() * 12) + 1;
-        $("#scorecard").text(blackSkull);
-    });
-
-    $("#red-skull").on("click", function(){
-        var redSkull = Math.floor(Math.random() * 12) + 1;
-        $("#scorecard").text(redSkull);
-    });*/
-
-
-});
