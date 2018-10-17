3ffa3e4e3d1ee75f8cbbb0d4ca63515b481224b3
David Downie <daviddownie@gmail.com>
2018-10-17 19:21:24 -0400

    numbers are adding together

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,10 +2,11 @@ $(document).ready(function(){
     
 var targetNumber = Math.floor(Math.random() * 101) + 19;
 var counter = 0;
-var skullImage 
+var skullImage
 var score 
 var wins = 0;
 var losses = 0;
+previous = 0;
 
 
 $("#target-number").html(targetNumber);
@@ -15,18 +16,20 @@ for(var i = 0; i < 4; i++){
     var randomNumbers = Math.floor(Math.random() * 12) + 1;
     skullImage = $("<img>");
     skullImage.attr({
-        "class": "skull agate black green red",
+        "class": "skull agate green black red",
         "data-random": randomNumbers
     });
 
     $("#skulls").append(skullImage);
 
-    $(".skull").on("click", function(){
+    $(".agate").on("click", function(){
         
-        var skullValue = ($(this).attr("data-random"));
-        skullValue = parseInt(skullValue);
+        var skullNum = parseInt($(this).attr("data-random"));
 
-        counter+= skullValue;
+        previous += skullNum;
+
+        console.log(previous);
+       
     })
 }
 
